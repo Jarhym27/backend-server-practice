a0e7a4e3fd005ef480c8c8d206975d41a1078b1f
Jarhym <jarhym.christoper@spaceforce.mil>
2023-03-14 15:51:01 -0600

    change to 10 lines or less

--- galvanize-backend/server.js
+++ galvanize-backend/server.js
@@ -1,18 +1,9 @@
 var http = require('http');
-const HOST = 'localhost';
-const PORT = 3001;
-
 const server = http.createServer((request, response) => {
-    let urll = request.url.split('/');
-        if (urll[1] === 'Home' ) {
           if (request.method === 'GET') {
             response.end("Hello Galvanize!");
           }
-        }
         response.statusCode = 404;
         response.end();
     });
-    
-      server.listen(3001, "localhost", () => {
-        console.log(`Server running at ${HOST}:${PORT}`);
-      });
+      server.listen(3001, "localhost", () => {console.log(`Server running at http://localhost:3000/`); });
